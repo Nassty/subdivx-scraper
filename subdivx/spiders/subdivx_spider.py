008ac3befd3f55accb4c214818544e13db108764
M G Berrotaran <nassty.phantassy@gmail.com>
2011-02-01 01:09:38 -0300

    restructured spider. deleted stupid .pyc files

--- subdivx/spiders/subdivx_spider.py
+++ subdivx/spiders/subdivx_spider.py
@@ -1,45 +1,47 @@
 from scrapy.spider import BaseSpider
+from scrapy.conf import settings
 from scrapy.selector import HtmlXPathSelector as Selector
 from subdivx.items import SubdivxItem
+from scrapy.http import Request
 import os
+import sys
 import urllib
 
 class SubdivxSpider(BaseSpider):
 
     name = "subdivx.com"
     allowed_domains = ["subdivx.com"]
-    search_therm = "x-files" 
+    search_therm = settings.get("serie", "x-files") 
     start = 0
     end = 60
-    start_urls = ["http://subdivx.com/index.php?accion=5&buscar=%s&masdesc=&idusuario=&nick=&oxfecha=&oxcd=&oxdown=&pg=%d" %(search_therm, x) for x in range(start, end)]
+    start_urls = ["http://subdivx.com/index.php?buscar=%s&accion=5&masdesc=&subtitulos=1&realiza_b=1" %search_therm]
 
+    def parse_subtitle(self, response, item):
+        filename = response.url.split("/")[-1]
+        local_name = "subs/%s.%s" %(item["title"],filename.split(".")[-1])
+        pos = 1
+        while os.path.exists(local_name):
+            local_name = "subs/%s-(%i).%s" %(item["title"], pos, filename.split(".")[-1])
+            pos += 1
+        local_file = open(local_name, "wb")
+        local_file.write(response.body)
+        local_file.close()
+        item["file"] = local_name
+        return item
 
     def parse(self, response):
         hxs = Selector(response)
         titles = hxs.select("//div[@id='menu_detalle_buscador']")
         contents = hxs.select("//div[@id='buscador_detalle']")
         items = []
+        next_link = hxs.select("//div[@class='pagination']/a[contains(text(), 'Siguiente')]/@href").extract()
+        if next_link:
+            yield Request("http://subdivx.com/" + next_link[0], callback=self.parse)
+
         for i in enumerate(titles):
             title = i[1].select("div/a/text()").extract()
             descr = contents[i[0]].select("div[@id='buscador_detalle_sub']/text()").extract()
-            external_file = urllib.urlopen(contents[i[0]].select("div[@id='buscador_detalle_sub_datos']/a[@target='new']/@href").extract()[0])
-
-            self.log("downloading %s (%s)" %(title, external_file.geturl()))
-
-            filename = external_file.geturl().split("/")[-1]
-            local_name = "subs/%s.%s" %(title[0],filename.split(".")[-1])
-            pos = 1
-            while os.path.exists(local_name):
-                local_name = "subs/%s-(%i).%s" %(title[0], pos, filename.split(".")[-1])
-                pos += 1
-            local_file = open(local_name, "wb")
-            local_file.write(external_file.read())
-            external_file.close()
-            local_file.close()
-
             item = SubdivxItem()
-            item["title"] = title
-            item["descr"] = descr
-            item["file"] = local_name 
-            items.append(item)
-        return items
+            item["title"] = title[0] if title else None
+            item["descr"] = descr[0] if descr else None
+            yield Request(contents[i[0]].select("div[@id='buscador_detalle_sub_datos']/a[@target='new']/@href").extract()[0], callback=lambda x: self.parse_subtitle(x, item))
